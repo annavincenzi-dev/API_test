b24d7f8df77d79c2f9d7923115afccba4d3d4795
Anna Vincenzi <vincenzi.anna.41@gmail.com>
2025-06-15 22:01:38 +0200

    Model Validator Service created

---
+++ app/Services/ModelValidatorService.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Support\Facades\Validator;
+
+class ModelValidatorService{
+    public function validate($model, $data, $updating = false){
+        
+        $rules = $model::recordValidator($data, $updating);
+        $messages = $model::recordValidatorMessages();
+
+        $validator = Validator::make($data, $rules, $messages);
+
+        //se la validazione fallisce, ritorno il validator per visualizzarne i messaggi.
+        if ($validator->fails()) {
+            return $validator;
+        }else{
+            return null;
+        }
+    }
+}
